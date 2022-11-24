f631da7990cb89c11954b498ab7d87a62fcd9906
fruzelee <fazlerabbicse@gmail.com>
2022-11-24 17:49:17 +0600

    initial boilerplate

---
+++ app/src/main/java/com/boilerplate/android_clean_architecture_mvvm_jetpack/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.boilerplate.android_clean_architecture_mvvm_jetpack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
