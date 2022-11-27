27c128dd49f7f40ae8ab9599c105f7ce9dd74958
fruzelee <fazle.rabbi.cse@gmail.com>
2022-11-27 16:25:31 +0600

    added product card adapter and it's functionality

---
+++ app/src/main/java/com/boilerplate/android_clean_architecture_mvvm_jetpack/ProductCardListAdapter.kt
@@ -0,0 +1,37 @@
+package com.boilerplate.android_clean_architecture_mvvm_jetpack
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.boilerplate.android_clean_architecture_mvvm_jetpack.databinding.ProductCardBinding
+
+class ProductCardListAdapter : RecyclerView.Adapter<ProductCardListAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.product_card, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind()
+    }
+
+    override fun getItemCount(): Int {
+        return 3
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind() {
+            val bind = ProductCardBinding.bind(itemView)
+            bind.viewProductName.text = "Product 1"
+            bind.viewProductDescription.text = "Description of product"
+            bind.productPrice.text = "100 US$"
+        }
+    }
+
+}
