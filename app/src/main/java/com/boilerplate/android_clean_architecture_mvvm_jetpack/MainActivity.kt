9a428c48cdc854c00f130770259af9150a41fc14
fruzelee <fazlerabbicse@gmail.com>
2022-11-27 16:25:31 +0600

    added product card adapter and it's functionality

--- app/src/main/java/com/boilerplate/android_clean_architecture_mvvm_jetpack/MainActivity.kt
+++ app/src/main/java/com/boilerplate/android_clean_architecture_mvvm_jetpack/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.boilerplate.android_clean_architecture_mvvm_jetpack
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.boilerplate.android_clean_architecture_mvvm_jetpack.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val adapter = ProductCardListAdapter()
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.viewProductList.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        binding.viewProductList.adapter = adapter
     }
 }
