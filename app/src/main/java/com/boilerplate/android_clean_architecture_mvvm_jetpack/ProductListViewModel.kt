45321dbd3e8a32e01c8c3c78e93387a78a3ef677
fruzelee <fazlerabbicse@gmail.com>
2022-12-07 12:05:54 +0600

    - feat: implement coroutines with mock data

--- app/src/main/java/com/boilerplate/android_clean_architecture_mvvm_jetpack/ProductListViewModel.kt
+++ app/src/main/java/com/boilerplate/android_clean_architecture_mvvm_jetpack/ProductListViewModel.kt
@@ -3,22 +3,30 @@ package com.boilerplate.android_clean_architecture_mvvm_jetpack
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
 
 class ProductListViewModel : ViewModel() {
 
     private val _viewState = MutableLiveData<ProductListViewState>()
     val viewState: LiveData<ProductListViewState> get() = _viewState
+    private val repository = ProductRepository()
 
     fun loadProductList() {
-        _viewState.postValue(ProductListViewState.Loading)
-        // Data call to fetch products
-        _viewState.postValue(ProductListViewState.Content((1..3).map {
-            ProductCardViewState(
-                "Playstation $it",
-                "This is a nice console! Check it out",
-                "200US$"
-            )
-        }))
+        viewModelScope.launch {
+            _viewState.postValue(ProductListViewState.Loading)
+            // Data call to fetch products
+            val productList = repository.getProductList()
+            _viewState.postValue(ProductListViewState.Content(productList))
+
+            /*_viewState.postValue(ProductListViewState.Content((1..3).map {
+                ProductCardViewState(
+                    "Playstation $it",
+                    "This is a nice console! Check it out",
+                    "200US$"
+                )
+            }))*/
+        }
     }
 }
