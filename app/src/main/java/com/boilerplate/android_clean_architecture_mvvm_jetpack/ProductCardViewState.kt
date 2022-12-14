ac41386b593e609389f36efae0e9f6d70ccce96c
fruzelee <fazlerabbicse@gmail.com>
2022-12-14 16:52:56 +0600

    - refactor: reformat the product card view state data class

--- app/src/main/java/com/boilerplate/android_clean_architecture_mvvm_jetpack/ProductCardViewState.kt
+++ app/src/main/java/com/boilerplate/android_clean_architecture_mvvm_jetpack/ProductCardViewState.kt
@@ -1,3 +1,7 @@
 package com.boilerplate.android_clean_architecture_mvvm_jetpack
 
-data class ProductCardViewState(val title: String, val description: String, val price: String)
+data class ProductCardViewState(
+    val title: String,
+    val description: String,
+    val price: String
+)
