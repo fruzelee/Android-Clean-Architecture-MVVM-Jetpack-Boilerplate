1f785f2259af4160bb2562b63572919fb58be355
fruzelee <fazlerabbicse@gmail.com>
2022-12-14 16:49:50 +0600

    - feat: add product entity class to represent the product which is going to be used by the   repository layer and the view state is going to use by the view layer, the ui layer

---
+++ app/src/main/java/com/boilerplate/android_clean_architecture_mvvm_jetpack/ProductEntity.kt
@@ -0,0 +1,8 @@
+package com.boilerplate.android_clean_architecture_mvvm_jetpack
+
+data class ProductEntity(
+    val title: String,
+    val description: String,
+    val price: Double,
+    val imageUrl: String
+)
