b69c7b1658347dd6532b390ede62e460784abf51
fruzelee <fazle.rabbi.cse@gmail.com>
2022-12-14 16:51:24 +0600

    - feat: get a reference to our client on product repository instead of returning mock data

--- app/src/main/java/com/boilerplate/android_clean_architecture_mvvm_jetpack/ProductRepository.kt
+++ app/src/main/java/com/boilerplate/android_clean_architecture_mvvm_jetpack/ProductRepository.kt
@@ -1,21 +1,25 @@
 package com.boilerplate.android_clean_architecture_mvvm_jetpack
 
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
 
 class ProductRepository {
 
+    private val client = ApiClient().getClient()
+
     suspend fun getProductList(): List<ProductCardViewState> {
         return withContext(Dispatchers.IO) {
-            delay(2000)
+            /*delay(2000)
             (1..3).map {
                 ProductCardViewState(
                     "Playstation $it",
                     "This is a nice console! Check it out",
                     "200US$"
                 )
-            }
+            }*/
+
+            //get a reference to our client instead of returning mock data
+            client.getProductList()
         }
     }
 }
