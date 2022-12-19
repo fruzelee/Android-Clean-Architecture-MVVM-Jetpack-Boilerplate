d5b530084ffdeef8188c3608a40ed9ab6a1e12bc
fruzelee <fazlerabbicse@gmail.com>
2022-12-19 11:33:48 +0600

    - feat: add new parameter imageUrl for ProductCardViewState data class

--- app/src/main/java/com/boilerplate/android_clean_architecture_mvvm_jetpack/ProductCardViewState.kt
+++ app/src/main/java/com/boilerplate/android_clean_architecture_mvvm_jetpack/ProductCardViewState.kt
@@ -3,5 +3,6 @@ package com.boilerplate.android_clean_architecture_mvvm_jetpack
 data class ProductCardViewState(
     val title: String,
     val description: String,
-    val price: String
+    val price: String,
+    val imageUrl: String
 )
