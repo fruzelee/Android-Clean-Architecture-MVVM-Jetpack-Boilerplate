d8680bb69754d5dfe2c28b1b22c79619b3f47e2d
fruzelee <fazle.rabbi.cse@gmail.com>
2022-12-20 11:59:56 +0600

    - fix: create an interface & make sure that our view model is using interface rather than concrete   class to comply with the DIP of SOLID principles

--- app/src/main/java/com/boilerplate/android_clean_architecture_mvvm_jetpack/ProductRepository.kt
+++ app/src/main/java/com/boilerplate/android_clean_architecture_mvvm_jetpack/ProductRepository.kt
@@ -1,32 +1,5 @@
 package com.boilerplate.android_clean_architecture_mvvm_jetpack
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-
-class ProductRepository {
-
-    private val client = ApiClient().getClient()
-
-    suspend fun getProductList(): List<ProductCardViewState> {
-        return withContext(Dispatchers.IO) {
-            /*delay(2000)
-            (1..3).map {
-                ProductCardViewState(
-                    "Playstation $it",
-                    "This is a nice console! Check it out",
-                    "200US$"
-                )
-            }*/
-
-            //get a reference to our client instead of returning mock data
-            client.getProductList().map {
-                ProductCardViewState(
-                    it.title,
-                    it.description,
-                    "TK ${it.price * 104.59}", //1 US Dollar = 104.59 Taka Dec 19, 2:01AM UTC
-                    it.imageUrl
-                )
-            }
-        }
-    }
+interface ProductRepository {
+    suspend fun getProductList(): List<ProductCardViewState>
 }
