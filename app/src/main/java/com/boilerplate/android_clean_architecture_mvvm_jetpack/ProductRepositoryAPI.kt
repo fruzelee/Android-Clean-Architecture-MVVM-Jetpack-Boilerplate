f96e2f74ce39a6987033b0a79d76de1c08b76569
fruzelee <fazle.rabbi.cse@gmail.com>
2022-12-20 12:01:42 +0600

    - feat: implements an interface on ProductRepositoryAPI class and override the method

---
+++ app/src/main/java/com/boilerplate/android_clean_architecture_mvvm_jetpack/ProductRepositoryAPI.kt
@@ -0,0 +1,32 @@
+package com.boilerplate.android_clean_architecture_mvvm_jetpack
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class ProductRepositoryAPI: ProductRepository {
+
+    private val client = ApiClient().getClient()
+
+    override suspend fun getProductList(): List<ProductCardViewState> {
+        return withContext(Dispatchers.IO) {
+            /*delay(2000)
+            (1..3).map {
+                ProductCardViewState(
+                    "Playstation $it",
+                    "This is a nice console! Check it out",
+                    "200US$"
+                )
+            }*/
+
+            //get a reference to our client instead of returning mock data
+            client.getProductList().map {
+                ProductCardViewState(
+                    it.title,
+                    it.description,
+                    "TK ${it.price * 104.59}", //1 US Dollar = 104.59 Taka Dec 19, 2:01AM UTC
+                    it.imageUrl
+                )
+            }
+        }
+    }
+}
