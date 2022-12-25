9e2ee29252da09a4a733454ca0e72197eabcc873
fruzelee <fazlerabbicse@gmail.com>
2022-12-25 11:41:14 +0600

    - feat: use @HiltViewModel annotations for the view models

--- app/src/main/java/com/boilerplate/android_clean_architecture_mvvm_jetpack/ProductListViewModel.kt
+++ app/src/main/java/com/boilerplate/android_clean_architecture_mvvm_jetpack/ProductListViewModel.kt
@@ -4,9 +4,11 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 class ProductListViewModel @Inject constructor(private val repository: ProductRepository) :
     ViewModel() {
 
