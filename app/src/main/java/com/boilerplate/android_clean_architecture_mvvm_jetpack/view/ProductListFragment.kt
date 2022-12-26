e443fa58244dd75dfb89cd647492757e763be4b8
fruzelee <fazle.rabbi.cse@gmail.com>
2022-12-26 14:52:03 +0600

    - feat: add new product list module

---
+++ app/src/main/java/com/boilerplate/android_clean_architecture_mvvm_jetpack/view/ProductListFragment.kt
@@ -0,0 +1,66 @@
+package com.boilerplate.android_clean_architecture_mvvm_jetpack.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.boilerplate.android_clean_architecture_mvvm_jetpack.ProductCardListAdapter
+import com.boilerplate.android_clean_architecture_mvvm_jetpack.ProductListViewModel
+import com.boilerplate.android_clean_architecture_mvvm_jetpack.ProductListViewState
+import com.boilerplate.android_clean_architecture_mvvm_jetpack.databinding.FragmentProductListBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ProductListFragment : Fragment() {
+
+    private lateinit var binding: FragmentProductListBinding
+    private val viewModel: ProductListViewModel by viewModels()
+    private val adapter = ProductCardListAdapter()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentProductListBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.viewProductList.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        binding.viewProductList.adapter = adapter
+        // using observable pattern to update the ui
+        viewModel.viewState.observe(viewLifecycleOwner) { viewState ->
+            updateUI(viewState)
+        }
+        viewModel.loadProductList()
+    }
+
+    private fun updateUI(viewState: ProductListViewState) {
+        when (viewState) {
+            is ProductListViewState.Content -> {
+                binding.viewProductList.isVisible = true
+                binding.errorView.isVisible = false
+                binding.loadingView.isVisible = false
+                adapter.setData(viewState.productList)
+            }
+            ProductListViewState.Error -> {
+                binding.viewProductList.isVisible = false
+                binding.errorView.isVisible = true
+                binding.loadingView.isVisible = false
+            }
+            ProductListViewState.Loading -> {
+                binding.viewProductList.isVisible = false
+                binding.errorView.isVisible = false
+                binding.loadingView.isVisible = true
+            }
+        }
+    }
+}
