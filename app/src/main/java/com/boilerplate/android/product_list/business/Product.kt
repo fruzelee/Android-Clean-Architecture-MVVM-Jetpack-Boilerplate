f5b9f34ca2cee3732f510f56469c0d4de870f53a
fruzelee <fazlerabbicse@gmail.com>
2023-01-29 11:25:21 +0600

    - feat: add new data class Product under business layer of 'product_list' feature

---
+++ app/src/main/java/com/boilerplate/android/product_list/business/Product.kt
@@ -0,0 +1,8 @@
+package com.boilerplate.android.product_list.business
+
+data class Product(
+    val title: String,
+    val description: String,
+    val price: Double,
+    val imageUrl: String
+)
