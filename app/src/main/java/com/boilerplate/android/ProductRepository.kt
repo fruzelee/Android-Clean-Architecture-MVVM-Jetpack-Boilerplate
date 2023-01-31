49fdac3dc4cbec1a9b2d2cba833f13c64c90827a
fruzelee <fazlerabbicse@gmail.com>
2023-01-31 16:55:24 +0600

    - feat(ProductRepository): create abstract getProductDetails suspend function

--- app/src/main/java/com/boilerplate/android/ProductRepository.kt
+++ app/src/main/java/com/boilerplate/android/ProductRepository.kt
@@ -1,7 +1,9 @@
 package com.boilerplate.android
 
+import com.boilerplate.android.product_details.business.ProductDetails
 import com.boilerplate.android.product_list.business.Product
 
 interface ProductRepository {
     suspend fun getProductList(): List<Product>
+    suspend fun getProductDetails(productId: String): ProductDetails
 }
