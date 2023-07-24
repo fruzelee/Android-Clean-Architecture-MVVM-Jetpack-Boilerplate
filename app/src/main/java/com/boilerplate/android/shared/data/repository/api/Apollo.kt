622c18d22263c97561e264e879ad5264ae83a230
fruzelee <fazlerabbicse@gmail.com>
2023-07-24 12:28:03 +0600

    feat: Add apolloClient to api repository package in shared data

---
+++ app/src/main/java/com/boilerplate/android/shared/data/repository/api/Apollo.kt
@@ -0,0 +1,38 @@
+package com.boilerplate.android.shared.data.repository.api
+
+import android.content.Context
+import com.apollographql.apollo3.ApolloClient
+import com.apollographql.apollo3.network.okHttpClient
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
+
+private var instance: ApolloClient? = null
+
+fun apolloClient(context: Context): ApolloClient {
+    if (instance != null) {
+        return instance!!
+    }
+
+    val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(AuthorizationInterceptor(context))
+        .build()
+
+    instance = ApolloClient.Builder()
+        .serverUrl("https://devapiv2.walcart.com/graphql")
+        .webSocketServerUrl("wss://devapiv2.walcart.com/graphql")
+        .okHttpClient(okHttpClient)
+        .build()
+
+    return instance!!
+}
+
+private class AuthorizationInterceptor(val context: Context) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request().newBuilder()
+//            .addHeader("Authorization", User.getToken(context) ?: "")
+            .build()
+
+        return chain.proceed(request)
+    }
+}
